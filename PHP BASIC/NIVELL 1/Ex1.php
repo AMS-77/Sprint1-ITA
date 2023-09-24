0bb8864c8c1992dde4e162f5dec38d797cad3b24
Fulck <ams77@outlook.es>
2023-09-24 16:25:48 +0200

    v1 exercici 1 NIVELL 1 / PHP BASIC

---
+++ PHP BASIC/NIVELL 1/Ex1.php
@@ -0,0 +1,19 @@
+<?php
+/*Exercici 1--> Defineix una variable de cada tipus: integer, double, string i boolean. Imprimeix-les per pantalla.
+Després crea una constant que inclogui el teu nom i mostra-ho en format títol per pantalla.*/
+
+    $num = 3;
+    $numD = 1.83;
+    $paraula = "casa";
+    $esPar = "TRUE";
+    define ( "NOM" , "Toni");
+
+    /*Si treballem amb el Terminal hauriem de posar la etiqueta \n pel salt de linia:
+    echo $num . "\n" . $numD . "\n" . $paraula . "\n" . $esPar;
+    Pero treballem amb el navegador i posem BR pel salt de linia:" */
+    echo $num . "<br>" . $numD . "<br>" . $paraula . "<br>" . $esPar;
+
+    //mostrem per pantalla el valor de la constant, concatenem les etiquetes h1 entre cometes (dobles o simples)
+    echo '<h1>' . NOM . '</h1>';
+
+?>
